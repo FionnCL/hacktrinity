a8c5f11fcb27394f1a464f75cb650bcd10589e26
camachof <camacholenihanfionn@gmail.com>
2024-02-25 00:49:13 +0000

    fixed stuff

--- lunchtime/src/components/Landing.js
+++ lunchtime/src/components/Landing.js
@@ -14,12 +14,14 @@ export default function Landing(){
             </div>
             <div className="circle"></div>
             <div className="circle-2"></div>
-            <div className="timetable">
-                <img src={timetable} alt="timetable" className="timetable"/>
-            </div>
-            <div className="landing-text-low">
-                <h1 className="h1-3">How It Works:</h1>
-                <h2 className="h3-1">Welcome to lunchtime, where syncing calendars with friends, clubs, or projects is a breeze. Simply import your Google or Apple Calendar, add contacts, and instantly find overlapping free time slots for meetings, hangouts, or events. Effortlessly coordinate schedules with ease!</h2>
+            <div className="timetable-lowtext">
+                <div className="timetable">
+                    <img src={timetable} alt="timetable" className="timetable"/>
+                </div>
+                <div className="landing-text-low">
+                    <h1 className="h1-3">How It Works:</h1>
+                    <h2 className="h3-1">Welcome to lunchtime, where syncing calendars with friends, clubs, or projects is a breeze. Simply import your Google or Apple Calendar, add contacts, and instantly find overlapping free time slots for meetings, hangouts, or events. Effortlessly coordinate schedules with ease!</h2>
+                </div>
             </div>
             <div className="circle-3"></div>
         </div>
